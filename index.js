333060af0498682a5c37c6cd0a6089154dfca5ae
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-01-12 19:51:18 -0600

    passes all tests

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const input = document.getElementById('button');
+function addingEventListener () {
+  const input = document.getElementById('button');
 
 function clickAlert() {
   alert('I was clicked!');
+  }
+  input.addEventListener('click', clickAlert);
 }
-
-input.addEventListener('click', clickAlert);
+addingEventListener();
